8725b925fcec75a81a8ef6606102f6590cd14871
MuriloSG <115160124+MuriloSG@users.noreply.github.com>
2023-09-11 12:27:34 -0300

    Add errors middleware

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
-import { Router } from 'express'
+import { AppError } from '@shared/errors/AppError'
+import { Request, Response, Router } from 'express'
 
 const routes = Router()
 
-routes.get('/', (request, response) => {
+routes.get('/', (request: Request, response: Response) => {
+  throw new Error('acesso negado')
   response.json({ messge: 'Olá dev' })
 })
 
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,15 +1,33 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
 import cors from 'cors'
 import 'dotenv/config'
 
 import { routes } from './routes'
+import { AppError } from '@shared/errors/AppError'
+
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
 app.use(routes)
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      })
+    }
+    console.log(error)
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal serve error',
+    })
+  },
+)
 app.listen(process.env.PORT, () => {
   console.log(`Servidor rodando na porta:${process.env.PORT}`)
 })
