5b0cfd9f016f5d092ffc6382d306a81b799ecc6e
MuriloSG <115160124+MuriloSG@users.noreply.github.com>
2023-09-11 11:20:21 -0300

    add eslint , editor confi and prettier

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,6 @@
 export class AppServer {
-    private app: string;
-
-    constructor(info: string){
-        this.app = info ?? 'Olá dev';
-    }
-};
+  private app: string
+  constructor(info: string) {
+    this.app = info ?? 'Olá dev'
+  }
+}
