bfa6f19c572f22df830cf7415aa98a457b9cf82e
MuriloSG <115160124+MuriloSG@users.noreply.github.com>
2023-09-11 16:11:32 -0300

    Create findByName method

--- src/roles/http/routes/roles.route.ts
+++ src/roles/http/routes/roles.route.ts
@@ -6,6 +6,10 @@ const rolesRepository = new RolesRepository()
 
 rolesRouter.post('/', (request: Request, response: Response) => {
   const { name } = request.body
+  const roleAlreadExists = rolesRepository.findByName(name)
+  if (roleAlreadExists) {
+    return response.status(400).json({ error: 'Role already exist' })
+  }
   const role = rolesRepository.create({ name })
   return response.status(201).json(role)
 })
