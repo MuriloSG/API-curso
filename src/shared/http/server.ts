512f7ce2e0015451e7991e8f0a5cc963153f480f
MuriloSG <115160124+MuriloSG@users.noreply.github.com>
2023-09-11 12:02:15 -0300

    config routes

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,14 +2,14 @@ import express from 'express'
 import 'express-async-errors'
 import cors from 'cors'
 import 'dotenv/config'
+
+import { routes } from './routes'
 const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.get('/', (request, response) => {
-  response.json({ messge: 'Olá dev' })
-})
 
+app.use(routes)
 app.listen(process.env.PORT, () => {
   console.log(`Servidor rodando na porta:${process.env.PORT}`)
 })
