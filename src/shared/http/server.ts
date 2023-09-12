f0e75f5982f43cfcad3c86e598634e0b1337be4b
MuriloSG <115160124+MuriloSG@users.noreply.github.com>
2023-09-12 13:49:11 -0300

    Config dataBase

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,34 +1,10 @@
-import express, { NextFunction, Request, Response } from 'express'
-import 'express-async-errors'
-import swaggerUi from 'swagger-ui-express'
-import cors from 'cors'
 import 'dotenv/config'
-import { routes } from './routes'
-import { AppError } from '@shared/errors/AppError'
-import swaggerFile from '../../swagger.json'
+import 'reflect-metadata'
+import { app } from './app'
+import { dataSource } from '@shared/typeorm'
 
-const app = express()
-
-app.use(express.json())
-app.use(cors())
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
-app.use(routes)
-
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
-        status: 'error',
-        message: error.message,
-      })
-    }
-    console.log(error)
-    return response.status(500).json({
-      status: 'error',
-      message: 'Internal serve error',
-    })
-  },
-)
-app.listen(process.env.PORT, () => {
-  console.log(`Servidor rodando na porta:${process.env.PORT}`)
+dataSource.initialize().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Servidor rodando na porta:${process.env.PORT}`)
+  })
 })
