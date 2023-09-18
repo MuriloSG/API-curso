5d77bacfd0cb5771f8f865ef8b28b1fbf7949135
MuriloSG <115160124+MuriloSG@users.noreply.github.com>
2023-09-18 08:06:01 -0300

    Create migration CreateUsersTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,10 +1,11 @@
 import { DataSource } from 'typeorm'
 import { CreateRolesTable1694538345489 } from './migrations/1694538345489-CreateRolesTable'
 import { Role } from '@roles/entities/Role'
+import { CreateUsersTable1694817477694 } from './migrations/1694817477694-CreateUsersTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [Role],
-  migrations: [CreateRolesTable1694538345489],
+  migrations: [CreateRolesTable1694538345489, CreateUsersTable1694817477694],
 })
