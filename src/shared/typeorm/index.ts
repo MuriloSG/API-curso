47eaebfa0846d2b75a6800ee8416adce5d1568bf
MuriloSG <115160124+MuriloSG@users.noreply.github.com>
2023-09-25 20:44:49 -0300

    Create migration CreateRefreshTokenTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -4,6 +4,7 @@ import { Role } from '@roles/entities/Role'
 import { CreateUsersTable1694817477694 } from './migrations/1694817477694-CreateUsersTable'
 import { AddRoleIdToUsersTable1695035247663 } from './migrations/1695035247663-AddRoleIdToUsersTable'
 import { User } from '@user/entities/User'
+import { CreateRefreshTokenTable1695684654486 } from './migrations/1695684654486-CreateRefreshTokenTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
@@ -13,5 +14,6 @@ export const dataSource = new DataSource({
     CreateRolesTable1694538345489,
     CreateUsersTable1694817477694,
     AddRoleIdToUsersTable1695035247663,
+    CreateRefreshTokenTable1695684654486,
   ],
 })
