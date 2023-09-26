c935270bd4cd25667c28923b6cd100c98f5aca6e
MuriloSG <115160124+MuriloSG@users.noreply.github.com>
2023-09-25 21:21:23 -0300

    Create refreshToken interface

---
+++ src/users/repositories/IRefreshTokenRepository.ts
@@ -0,0 +1,18 @@
+import { RefreshToken } from '@user/entities/RefreshToken'
+
+type CreateRefreshTokenDTO = {
+  user_id: string
+  token: string
+  valid: boolean
+  expires: Date
+}
+export interface IRefreshTokenRepository {
+  create({
+    user_id,
+    token,
+    valid,
+    expires,
+  }: CreateRefreshTokenDTO): Promise<RefreshToken>
+  findByToken(token: string): Promise<RefreshToken | null>
+  invalidate(refresh_token: RefreshToken): Promise<void>
+}
